26e40d1d25aeed9b06a1ba469be7da2ad36c6fb5
Andrew Reeves <areeves42@gmail.com>
2022-08-30 02:27:41 -0400

    Rework /numbers flow a bit to clean up state transition logic

--- pages/numbers.tsx
+++ pages/numbers.tsx
@@ -2,78 +2,108 @@ import { useReducer, useState, useEffect } from 'react'
 
 import Result from '../components/result'
 
-const initialState = {
-  phase: 'playing',
+const CIRCLE = '\u25EF'
+
+interface numbersReducerState {
+  phase: 'pre-round'|'playing'|'correct'|'incorrect'|'reset',
+  level: number,
+  objects: string[],
+  remaining: string[][]
+}
+interface numbersReducerSimpleAction { type: 'begin'|'continue' }
+interface numbersReducerAnswerAction { type: 'answer', value: number }
+interface numbersReducerResetAction { type: 'reset', value: string[][] }
+type numbersReducerAction = numbersReducerSimpleAction | numbersReducerResetAction | numbersReducerAnswerAction
+
+const initialState: numbersReducerState = {
+  phase: 'reset',
   level: 1,
-  remaining: [],
-  objects: [ '\u25EF' ],
+  remaining: [ ],
+  objects: [ ],
+}
+
+const reducer = (state: numbersReducerState, action: numbersReducerAction): numbersReducerState => {
+  // level generation handled by an effect which passes in the remaining rounds
+  if(state.phase === 'reset' && action.type === 'reset') {
+    return { ...state, phase: 'pre-round', objects: [], remaining: action.value }
+  }
+  // begin, pre-round -> playing (triggered by usePreroundEffect)
+  if(state.phase === 'pre-round' && action.type === 'begin') {
+    return {
+      phase: 'playing',
+      level: state.level,
+      objects: state.remaining[0],
+      remaining: state.remaining.slice(1)
+    }
+  }
+  // answer, playing -> correct/incorrect
+  if(state.phase === 'playing' && action.type === 'answer') {
+    const isCorrect = action.value === state.objects.length
+    return { ...state, phase: isCorrect ? 'correct' : 'incorrect' }
+  }
+  // continue, correct -> pre-round/ reset (lvl+1)
+  if(action.type === 'continue' && state.phase === 'correct') {
+    if(state.remaining.length) {
+      return { ...state, phase: 'pre-round' }
+    } else {
+      return { ...state, phase: 'reset', level: state.level + 1 }
+    }
+  }
+  // continue, incorrect -> reset
+  if( action.type === 'continue' && state.phase === 'incorrect' ) {
+    return { ...state, phase: 'reset' }
+  }
+
+  console.warn('unexpected reducer action/state', {state, action})
+  return state
+}
+
+const useResetEffect = (state, dispatch) => {
+  useEffect(
+    () => {
+      const rounds = Array(state.level)
+        .fill(0)
+        .map( (_,i) => i+1 )
+        .map( i => Array(i).fill(CIRCLE) )
+      // select rounds in random order to populate the level
+      const value = []
+      while(rounds.length) {
+        value.push( rounds.splice( Math.floor( Math.random() * rounds.length), 1)[0] )
+      }
+      dispatch( { 'type': 'reset', value } )
+    },
+    [state, dispatch]
+  )
+}
+
+const usePreRoundEffect = (state, dispatch, voiceControl) => {
+  useEffect(
+    () => {
+      if(state.phase === 'pre-round') {
+        voiceControl.current.say('How many circles?')
+        dispatch({type: 'begin'})
+      }
+    },
+    [state, dispatch]
+  )
 }
 
 export default function Numbers({ voiceControl }) {
-  const [message, setMessage] = useState(null)
-
-  const reducer = (state, action) => {
-    if(state.phase === 'playing' && typeof action === 'number') {
-      let {phase, level, remaining, objects} = state
-      remaining = remaining.slice() // copy values so we don't modify incoming state var
-      const resetLvl = () => {
-        remaining = Array(level).fill(0).map((_,i)=>i+1).sort(() => Math.random() - 0.5)
-      }
-      if(action === objects.length) {
-        // correct
-        phase = 'correct'
-        if(remaining.length === 0) {
-          // level complete
-          level++
-          resetLvl()
-        }
-      } else {
-        // incorrect
-        phase = 'incorrect'
-        resetLvl()
-      }
-      // next round
-      objects = Array(remaining.pop()).fill('\u25EF')
-      return {
-        phase,
-        level,
-        remaining,
-        objects,
-      }
-    }
-    if((state.phase === 'correct' || state.phase === 'incorrect') && action === 'continue') {
-      setMessage('How many circles?')
-      return {
-        ...state,
-        phase: 'playing',
-      }
-    }
-    console.warn({msg:'unexpected reducer action', state, action})
-    return state
-  }
-
   const [state, dispatch] = useReducer(reducer, initialState)
+  useResetEffect(state, dispatch )
+  usePreRoundEffect(state, dispatch, voiceControl)
 
-  useEffect(
-    () => {
-      if(message && voiceControl.current) {
-        voiceControl.current.say(message)
-        setMessage(null)
-      }
-    },
-    [message]
-  )
+  if(state.phase === 'correct' || state.phase === 'incorrect') {
+    return <Result isCorrect={state.phase === 'correct'} continueAction={() => dispatch({type: 'continue'} )} />
+  }
 
   const objects = state.objects.map((o,i) => <span className="p-4" key={i}>{o}</span>)
   const buttons = []
   for(let i = 1; i <= state.level; i++) {
-    const onClick = () => dispatch(i)
-    buttons.push( <button key={i} onClick={() => dispatch(i)} className="m-4 px-4 py-2 border rounded-xl">{i}</button> )
+    const onClick = () => dispatch({ type: 'answer', value: i })
+    buttons.push( <button key={i} onClick={onClick} className="m-4 px-4 py-2 border rounded-xl">{i}</button> )
   }
 
-  if(state.phase === 'correct' || state.phase === 'incorrect') {
-    return <Result isCorrect={state.phase === 'correct'} continueAction={() => dispatch('continue')} />
-  }
   return (
     <div className="flex flex-col items-center">
       <div className="flex m-4 text-6xl"> {objects} </div>
